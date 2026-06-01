c940b442461ecf0ec19c6cc730de291ea71f4a48
MasonFarmer135 <149623254+MasonFarmer135@users.noreply.github.com>
2026-06-01 18:53:30 +0100

    v2
    Display:
    • Added a main menu screen.
    
    Input handling:
    • Stored the users input in a variable and rendered text to match this value.

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 
-#pygame.
+#Pygame.
 pygame.init()
 
 #Application window settings.
@@ -11,6 +11,24 @@ window = pygame.display.set_mode(resolution)
 windowWidth = window.get_width()
 windowHeight = window.get_height()
 windowClock = pygame.time.Clock()
+windowDisplay = 1
+
+#Variables
+userInput = "NONE"
+
+#Font
+font1 = pygame.font.SysFont("arcade classic", 50, bold = False)
+
+#Text
+##Main Menu
+mainMenuText1 = font1.render('Click the "Enter" key.', True, ("#FFFFFF"))
+mainMenuText1Center = mainMenuText1.get_rect(center = (windowWidth / 2, windowHeight / 2 - 200))
+
+#Game
+gameText1 = font1.render('Click an "Arrow" key.', True, ("#FFFFFF"))
+gameText1Center = gameText1.get_rect(center = (windowWidth / 2, windowHeight / 2 - 200))
+gameText2 = font1.render('', True, ("#FFFFFF"))
+gameText2Center = gameText2.get_rect(center = (windowWidth / 2, windowHeight / 2))
 
 #Main Application
 while True:
@@ -18,9 +36,37 @@ while True:
         if (event.type == pygame.QUIT):
             sys.exit()
 
-    window.fill("#000000")
+        #Main menu
+        if (windowDisplay == 1):
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_RETURN):
+                    windowDisplay = 2
+
+        #Game
+        elif (windowDisplay == 2):
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_UP):
+                    userInput = "UP" 
+                elif (event.key == pygame.K_DOWN):
+                    userInput = "DOWN"
+                elif (event.key == pygame.K_LEFT):
+                    userInput = "LEFT"
+                elif (event.key == pygame.K_RIGHT):
+                    userInput = "RIGHT"
+
+    #Main menu
+    if (windowDisplay == 1):
+        window.fill("#000000")
+        window.blit(mainMenuText1, mainMenuText1Center)
+
+    #Game
+    if (windowDisplay == 2):
+        window.fill("#000000")
+        window.blit(gameText1, gameText1Center)
+        gameText2 = font1.render(userInput, True, ("#FFFFFF"))
+        gameText2Center = gameText2.get_rect(center = (windowWidth / 2, windowHeight / 2))
+        window.blit(gameText2, gameText2Center)
 
     #60 frames per second
     windowClock.tick(60)
     pygame.display.update()
-    
